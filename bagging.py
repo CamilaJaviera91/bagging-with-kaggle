177b6f517663cf3efb9495a00b7c808912f5abb1
camilajaviera <cjmunozn@hotmail.com>
2024-12-06 12:13:15 -0300

    feat(bagging): preprocess the data Prepare the dataset to make it suitable for modeling.

--- bagging.py
+++ bagging.py
@@ -25,3 +25,84 @@ print("\nSelect columns from the list:")
 for column in dataf.columns:
     print(f"- Column '{column}': type {dataf[column].dtype}")
 print("-" * 32)
+
+"""
+3. Preprocess the data
+Prepare the dataset to make it suitable for modeling.
+"""
+
+def select_columns():
+    """
+    Function to interactively select the columns to be used for analysis.
+    """
+    print("\nSelect the columns to be used")
+    print("-" * 32)
+    selected_columns = []
+    
+    while True:
+        col = input("Enter column name (or type 'e' to exit): ")
+        if col.lower() == "e":
+            break
+        else:
+            selected_columns.append(col)
+    
+    return selected_columns
+
+# Call the function to choose columns and create a subset of the data
+selected_columns = select_columns()
+subset_data = data[selected_columns]
+
+# Create a new DataFrame with the selected columns
+df = pd.DataFrame(subset_data)
+
+# Identify unique values in each column to decide how to encode or process the data
+print("\n")
+print("-" * 32)
+print("Unique values in columns:")
+for column in df.columns:
+    print(f"Column '{column}': {df[column].nunique()} unique values")
+
+# Identify columns with missing values
+print("\n")
+def identify_null_columns():
+    """
+    Function to identify columns with missing values.
+    """
+    columns_with_null = df.columns[df.isnull().any()].to_list()
+    print("-" * 32)
+    print("Columns with missing values:")
+    types_of_null_columns = df[columns_with_null].dtypes
+    return types_of_null_columns
+
+print(identify_null_columns())
+
+# Impute missing values
+for column in df:
+    # Impute missing values with the mean for numeric columns
+    if df[column].dtype in ['float64', 'int64']:
+        imputer = SimpleImputer(strategy='mean')
+        df[column] = imputer.fit_transform(df[column].values.reshape(-1, 1))
+        print(identify_null_columns())
+    else:
+        print(f"Column '{column}' is not numeric (int or float).")
+
+# Encode categorical variables with 2 or fewer unique values
+print("\n")
+print("-"*32)
+for column in df.columns:
+    if df[column].nunique() <= 2:
+        #Showing the original values
+        print(f"{column} : {df[column].unique()}")
+        encoder = LabelEncoder()
+        df[column] = encoder.fit_transform(df[column])
+        
+        #showing the encoded values
+        print(f"{column} : {df[column].unique()}")
+
+# User input for target column
+target_column = input("\nEnter the column to be used as the target variable: ")        
+
+# Split the data into features (X) and target (y)
+X = df.drop(target_column, axis=1)
+y = df[target_column]
+        
