e72fc9bab539dc1ddef2d991fae059a495ffa48e
camilajaviera <cjmunozn@hotmail.com>
2024-12-06 12:12:04 -0300

    feat(bagging): load and explore the dataset (load data using the kaggle_connect() function)

--- bagging.py
+++ bagging.py
@@ -9,3 +9,19 @@ from sklearn.metrics import accuracy_score           # Calculates the accuracy o
 from sklearn.impute import SimpleImputer             # Handles missing (NaN) values in the dataset
 from sklearn.preprocessing import LabelEncoder       # Encodes categorical labels (non-numeric variables) into numeric values
 from kaggle_connect import kaggle_connect            # Custom function to fetch the dataset using Kaggle API
+
+"""
+2. Load and explore the dataset
+Load data using the kaggle_connect() function
+"""
+data = kaggle_connect()
+dataf = pd.DataFrame(data)
+print(f"Loaded data with {data.shape[0]} rows and {data.shape[1]} columns.")
+print("-" * 32)
+print("\n")
+
+# Print the columns to understand the structure of the data
+print("\nSelect columns from the list:")
+for column in dataf.columns:
+    print(f"- Column '{column}': type {dataf[column].dtype}")
+print("-" * 32)
