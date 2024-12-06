70e5569b5d622f712e0e2d91e857d246242ceed2
camilajaviera <cjmunozn@hotmail.com>
2024-12-06 18:20:58 -0300

    feat(google): add warning and plt style.

--- google_sheets_utils.py
+++ google_sheets_utils.py
@@ -5,3 +5,8 @@ import warnings
 from google.oauth2 import service_account
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
+
+#Add warning and plt style
+warnings.filterwarnings('ignore')
+plt.style.use('default')
+
