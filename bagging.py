6a651a2d4fdc027285d8b0601b80b6974bed6f24
camilajaviera <cjmunozn@hotmail.com>
2024-12-10 13:31:05 -0300

    fix(main): change the return value.

--- bagging.py
+++ bagging.py
@@ -19,8 +19,8 @@ Load data using the kaggle_connect() function
 
 # Create a new function so we can access kaggle_connect file
 def run_kaggle_download():
-    rkd = curses.wrapper(kaggle_connect)
-    return rkd
+    # Wrapper function to run Kaggle connect using curses.
+    return curses.wrapper(kaggle_connect)
 
 data = run_kaggle_download()
 dataf = pd.DataFrame(data)
